ca173ceaa8c11521d09c272f36990241c9220c5e
David Wisecarver <dwisecar@gmail.com>
2020-10-21 16:13:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,12 @@
 def key_for_min_value(name_hash)
   smallest_value = 1000000;
   smallest_key = ""
-  name_hash.each do |key, value|
-    if name_hash == {}
+  
+   if name_hash == {}
       smallest_key = nil
     end
+  
+  name_hash.each do |key, value|
     
     if value < smallest_value
       smallest_value = value
